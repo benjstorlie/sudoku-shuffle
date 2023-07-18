b65d558f33586a9c3d27f7ce0ccaf773eb6ac5ae
bjs <127002178+benstorlie@users.noreply.github.com>
2023-07-17 23:15:37 -0700

    making Puzzle class for saving

---
+++ assets/puzzles/puzzleClass.js
@@ -0,0 +1,94 @@
+class Puzzle {
+  static blank = [
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+  ]
+
+  static copy(array) {
+    let newCopy = [];
+    try {
+      for (let row = 0; row < 9 ; row++) {
+        newCopy[row] = [];
+        for (let col = 0; col <9 ; col++) {
+          newCopy[row][col] = {
+            value: array[row][col].value,
+            possibles: array[row][col].possibles
+          }
+        }
+      }
+    } catch {
+      for (let row = 0; row < 9 ; row++) {
+        newCopy[row] = [];
+        for (let col = 0; col <9 ; col++) {
+          newCopy[row][col] = {
+            value: array[row][col],
+            possibles: []
+          }
+        }
+      }     
+    }
+    return newCopy;
+  }
+
+  // 
+  constructor(array) {
+    // validate array size
+    if (!array) {
+      array = Puzzle.blank;
+    } else if ( array.length !== 9 || array.some(x => x.length !== 9) ) {
+      throw new Error("Array must be 9 by 9.")
+    }
+    try {
+      this.id = crypto.randomUUID().slice(-4);
+    } catch {
+      this.id = String(new Date().getTime()).slice(-4);
+    }
+    this.stack = [];
+    this.puzzle = Puzzle.copy(array);
+
+  }
+
+  toString() {
+    let array = [];
+    for (let row = 0; row < 9 ; row++) {
+      array[row] = [];
+      for (let col = 0; col <9 ; col++) {
+        array[row][col] = this.puzzle[row][col].value ;
+      }
+      array[row] = JSON.stringify(array[row]);
+    }
+    return JSON.stringify(array,null,2);
+  }
+
+  setPuzzle() {
+    let timestamp = new Date().getTime();
+    this.stack.unshift(timestamp);
+    localStorage.setItem(`puzzle${id}-${timestamp}`,JSON.stringify(this.puzzle));
+  }
+
+  getPuzzle() {
+    let timestamp = this.stack.shift();
+    this.puzzle = JSON.parse(localStorage.getItem(`puzzle${id}-${timestamp}`))
+  }
+
+  build() {
+    for (let row = 0; row < 9 ; row++) {
+      for (let col = 0; col <9 ; col++) {
+        
+      }
+    }
+  }
+
+  save() {
+
+  }
+
+}
+
--- test.js
+++ test.js
@@ -1,14 +1,4 @@
 
-function notnot(a, b) {
-  return (a ? b : !b)
-}
 
-function xor(a , b) {
-  return (a ^ !b)
-}
-
-const values = [{a:1,b:1},{a:1,b:0},{a:0,b:1},{a:0,b:0}];
-
-values.forEach(({a,b}) => {
-  console.log("a",a,"b",b,"not",notnot(a,b),"xor",xor(a,b))
-});
+let x = crypto.randomUUID();
+console.log(x)
