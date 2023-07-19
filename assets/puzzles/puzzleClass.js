24492a1b4d86d6ac604ef0d12dcb3624029ccdcb
bjs <127002178+benstorlie@users.noreply.github.com>
2023-07-18 17:26:12 -0700

    save single move functions

--- assets/puzzles/puzzleClass.js
+++ assets/puzzles/puzzleClass.js
@@ -67,14 +67,15 @@ class Puzzle {
     return JSON.stringify(array,null,2);
   }
 
-  setPuzzle() {
+  setPuzzle(singleMove) {
     $("#coverscreen").addClass(".disable")
+    let puzzle = singleMove || this.puzzle;
     let timestamp = new Date().getTime();
     this.stack.unshift(timestamp);
     localStorage.setItem(
       `puzzle${id}-${timestamp}`,
       JSON.stringify(
-        this.puzzle,
+        puzzle,
         (k,v)=>{
           if (k==="possibles" && v instanceof Set){
             return Array.from(v).map(x => Number(x))
@@ -85,10 +86,28 @@ class Puzzle {
     $("#coverscreen").removeClass(".disable")
   }
 
+  saveSingleMove($cells) {
+    $("#coverscreen").addClass(".disable")
+    let saveCells = [];
+    $cells.each((index,cell) => {
+      const $cell = $(cell);
+      let {row,col} = $cell.data;
+      let cellValue = $cell.data("value");
+      let $candidates = $(`.candidate.row${row}.col${col}.possible`);
+      let possibles = new Set()
+      $candidates.each((index,candidate) => {
+        possibles.add($(candidate).data("value"))
+      })
+      saveCells[index] = {row,col,value:cellValue,possibles}
+    })
+    this.save({singleMove:saveCells});
+    $("#coverscreen").removeClass(".disable")
+  }
+
   getPuzzle() {
     $("#coverscreen").addClass(".disable")
     let timestamp = this.stack.shift();
-    this.puzzle = JSON.parse(
+    let puzzle = JSON.parse(
       localStorage.getItem(`puzzle${id}-${timestamp}`),
       (k,v)=>{
         if (k==="possibles" && Array.isArray(v)){
@@ -96,21 +115,70 @@ class Puzzle {
         }
       }
     );
+    if (!puzzle.hasOwnProperty("singleMove")) {
+      this.puzzle = puzzle;
+    } else {
+      const saveCells = puzzle.singleMove();
+      saveCells.forEach(cell => {
+        const {row,col,value,possibles} = cell;
+        this.puzzle[row][col]={value,possibles};
+      });
+    }
     $("#coverscreen").removeClass(".disable")
 
   }
 
-  fill() {
+  fill(saveCells) {
     $("#coverscreen").addClass(".disable")
-    $(".cell").removeClass("highlighted show-digit show-candidates");
-    $(".cell").children().removeClass("show hide possible eliminated");
-    for (let row = 0; row < 9 ; row++) {
-      for (let col = 0; col <9 ; col++) {
-        let $cell = $(`#cell-row${row}-col${col}`);
-        let $digit = $cell.children(".digit");
-        let $candidates = $cell.children(".candidate");
+    if (!saveCells) {
+      $(".cell").removeClass("highlighted show-digit show-candidates");
+      $(".cell").children().removeClass("show hide possible eliminated");
+      for (let row = 0; row < 9 ; row++) {
+        for (let col = 0; col <9 ; col++) {
+          let $cell = $(`#cell-row${row}-col${col}`);
+          let $digit = $cell.children(".digit");
+          let $candidates = $cell.children(".candidate");
 
-        let cellValue = this.puzzle[row][col].value;
+          let cellValue = this.puzzle[row][col].value;
+          if (cellValue) {
+            $cell.addClass('show-digit').attr("data-value",cellValue);
+            $digit.addClass("show").text(cellValue).attr("data-value",cellValue)
+            $candidates.addClass("hide")
+          } else {
+            $cell.addClass('show-candidates').attr("data-value","");
+            $digit.addClass("hide").text("").attr("data-value","")
+            $candidates.addClass("show")
+          }
+
+          /** * @type {Set<Number>} */
+          let possibles = this.puzzle[row][col].possibles;
+          let total = possibles.size;
+          if (total === 9) {
+            $candidates.addClass("possible");
+          } else if (total === 0) {
+            $candidates.addClass("eliminated")
+          } else {
+            for (let val = 1; val <=9; val++) {
+              const $candidate = $candidates.filter(`.val${val}`);
+              if (possibles.has(val)) {
+                $candidate.addClass("possible");
+                $cell.addclass(`possible-${val}`);
+              } else {
+                $candidate.addClass("eliminated");
+                $cell.removeclass(`possible-${val}`);
+              }
+            }
+          }
+        }
+      }
+    } else {
+      saveCells.forEach(cell => {
+        const {row,col,value,possibles} = cell;
+        let $cell = $(`#cell-row${row}-col${col}`).removeClass("highlighted show-digit show-candidates");
+        let $digit = $cell.children(".digit").removeClass("show hide");
+        let $candidates = $cell.children(".candidate").removeClass("show hide possible eliminated");
+
+        let cellValue = value;
         if (cellValue) {
           $cell.addClass('show-digit').attr("data-value",cellValue);
           $digit.addClass("show").text(cellValue).attr("data-value",cellValue)
@@ -121,8 +189,6 @@ class Puzzle {
           $candidates.addClass("show")
         }
 
-        /** * @type {Set<Number>} */
-        let possibles = this.puzzle[row][col].possibles;
         let total = possibles.size;
         if (total === 9) {
           $candidates.addClass("possible");
@@ -140,7 +206,7 @@ class Puzzle {
             }
           }
         }
-      }
+      });
     }
     $("#coverscreen").removeClass(".disable")
   }
@@ -152,7 +218,7 @@ class Puzzle {
       }
       let cellValue = $(`#cell-row${row}-col${col}`).attr("data-value");
       this.puzzle[row][col].value = (cellValue ? cellValue : 0);
-      
+
       let possibles = new Set()
       for (let val = 1; val <=9; val++) {
         let $candidate = $(`#candidate-row${row}-col${col}-val${val}`);
