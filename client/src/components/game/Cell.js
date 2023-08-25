5e85e4f811c3e38d4f22af6cf1b06f02b7a8e6fd
bjs <127002178+benstorlie@users.noreply.github.com>
2023-08-24 17:38:00 -0700

    more css for sudoku grid

--- client/src/components/game/Cell.js
+++ client/src/components/game/Cell.js
@@ -1,29 +1,48 @@
 import React, {useState} from 'react';
 import './Grid.css';
+import { iter, possibleCandidatesArr } from '../../utils/gameUtils'
 
 
 export default function Cell({row, col}) {
 
   const [value, setValue] = useState(0);
+  const [color, setColor] = useState('var(--sudoku-grid-bg)');
+  // *TODO* use useContext to set this value
+  const possibleSet = new Set([1,2,3])
+  const isPossibleArr = possibleCandidatesArr(possibleSet);
+
+  setColor(color === '' ? 'var(--sudoku-grid-bg)' : color);
 
   const box = 3*row + col;
 
-  // Grid positioning
-  const style = {
-    gridRow: (row + 1) + ' span 1',
-    gridColumn: (col + 1) + ' span 1',
-  }
+  return (
+    <div className="cell">
+      {value ? (
+        <Digit value={value} />
+      ) : (
+        iter(9).map((num) => (
+          <Candidate 
+            key={num} 
+            num={num} 
+            possible={isPossibleArr[num]}
+            color={color} 
+          />
+        ))
+      )}
+    </div>
+  );
+}
+
+function Digit({value}) {
+
+  // in css, grid-area: 1 / 1 / 4 / 4, to fill the cell
   
   return (
-    <div style={style} className="cell">
-
-    </div>
+    <div>{value ? value : ''}</div>
   )
 }
 
-function Candidate({num}) {
-
-  const [eliminated, setEliminated] = useState(false);
+function Candidate({num, color, possible}) {
 
   const row = (num-1) % 3;
   const col = (num-1 - 3*row);
@@ -32,17 +51,11 @@ function Candidate({num}) {
   const style = {
     gridRow: (row + 1) + ' span 1',
     gridColumn: (col + 1) + ' span 1',
+    color: possible ? 'var(--candidate-color)' : color
   }
 
   return (
-    <div style={style} className={`candidate ${eliminated ? 'hide' : 'show'}`}>
+    <div style={style} >
     </div>
-  )
-}
-
-function Digit({value}) {
-  
-  return (
-    <div className={`digit ${value ? "show" : "hide"}`}>{value ? value : ''}</div>
   )
 }
