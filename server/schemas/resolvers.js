efa16bcafa16154aa6d71f26ac02db785897e9c4
Pat Lowe <lowe-patrick@att.net>
2023-08-24 21:18:44 -0700

    addded JWT authorization functionality & some React Profile, Login, Sign up pages

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,18 +1,60 @@
-const { Game, User } = require('../models');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const { AuthenticationError } = require('apollo-server-express');
-const { signToken } = require('../utils/auth');
+const { Game, Profile } = require("../models");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 // Other imports...
-
 
 const resolvers = {
   Query: {
-    // Define query resolvers...
+    profiles: async () => {
+      return Profile.find();
+    },
+
+    profile: async (parent, { profileId }) => {
+      return Profile.findOne({ _id: profileId });
+    },
+    // By adding context to our query, we can retrieve the logged in user without specifically searching for them
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return Profile.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
   },
+
   Mutation: {
-    // Define mutation resolvers...
+    addProfile: async (parent, { name, email, password }) => {
+      const profile = await Profile.create({ name, email, password });
+      const token = signToken(profile);
+
+      return { token, profile };
+    },
+    login: async (parent, { email, password }) => {
+      const profile = await Profile.findOne({ email });
+
+      if (!profile) {
+        throw new AuthenticationError("No profile with this email found!");
+      }
+
+      const correctPw = await profile.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect password!");
+      }
+
+      const token = signToken(profile);
+      return { token, profile };
+    },
+
+    removeProfile: async (parent, args, context) => {
+      if (context.user) {
+        return Profile.findOneAndDelete({ _id: context.user._id });
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+    // Make it so a logged in user can only remove a skill from their own profile
   },
-}
+};
 
 module.exports = resolvers;
