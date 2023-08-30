70d04d9bf719e7274f16019b442995d326d4c564
Karelyn Rojas <knrojas@ucdavis.edu>
2023-08-29 19:14:08 -0700

    Timer js created and in progress

---
+++ client/src/pages/Timer.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Countdown from "react-countdown";
+
+// Random component
+const Completionist = () => <span>Times out!</span>;
+const Pause = () => <span>Timer Paused!</span>;
+
+// Renderer callback with condition
+const renderer = ({ hours, minutes, seconds, completed }) => {
+  if (completed) {
+    // Render a complete state
+    return <Completionist />;
+  } else {
+    // Render a countdown
+    return (
+      <span>
+        {hours}:{minutes}:{seconds}
+      </span>
+    );
+  }
+ 
+  
+};
+
+ReactDOM.render(
+  <Countdown date={Date.now() + 600000} renderer={renderer}/>,
+  document.getElementById("root")
+);
