0f6866cb2190d2582f8c374a802e5d91aa690c5b
Pat Lowe <lowe-patrick@att.net>
2023-09-05 13:56:31 -0700

    changed styling

--- client/src/components/nav/NewGame.js
+++ client/src/components/nav/NewGame.js
@@ -1,21 +1,33 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup'
-import Card from 'react-bootstrap/Card'
-import { useGameContext } from '../../utils/GameContext';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Card from "react-bootstrap/Card";
+import { useGameContext } from "../../utils/GameContext";
 
-export default function NewGame({className}) {
-
+export default function NewGame({ className }) {
   const { loadDifficulty } = useGameContext();
 
   return (
-    <Card bg="info" id="new-game" className='pt-2' style={{minWidth: 'fit-content'}}>
-      <Card.Title style={{textAlign:'center'}}>Start New Game</Card.Title>
-        <ButtonGroup size='lg' className={className}>
-    <Button variant='success' onClick={() => loadDifficulty('easy')}>Easy</Button>
-    <Button variant='warning' onClick={() => loadDifficulty('medium')} >Medium</Button>
-    <Button variant='danger' onClick={() => loadDifficulty('hard')} >Hard</Button>
-  </ButtonGroup>
+    <Card
+      bg="secondary"
+      id="new-game"
+      className="pt-2m-2 border border-white"
+      style={{ minWidth: "fit-content" }}
+    >
+      <Card.Title className="text-white" style={{ textAlign: "center" }}>
+        Start New Game
+      </Card.Title>
+      <ButtonGroup size="lg" className={className}>
+        <Button variant="success" onClick={() => loadDifficulty("easy")}>
+          Easy
+        </Button>
+        <Button variant="warning" onClick={() => loadDifficulty("medium")}>
+          Medium
+        </Button>
+        <Button variant="danger" onClick={() => loadDifficulty("hard")}>
+          Hard
+        </Button>
+      </ButtonGroup>
     </Card>
-  )
+  );
 }
